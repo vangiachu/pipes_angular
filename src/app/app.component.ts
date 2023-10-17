a59371c7cc90f2c76aeeb6e3f9bfbca97e045d72
Chu Văn Giá <vangiachu@gmail.com>
2023-10-17 10:27:55 +0700

    complete commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,43 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'pipes_angular';
+  name = '';
+  date = '';
+  amount = 0;
+  height = 0;
+  miles =0;
+
+  car = {
+    make: 'Toyota',
+    model: 'Camry',
+    year: 2000
+  };
+
+  onMilesChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+    this.miles = parsedValue;
+  }
+  
+  onHeightChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+    this.height = parsedValue;
+  }
+
+  onNameChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.name = target.value;
+  }
+
+  onDateChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    this.date = target.value;
+  }
+
+  onAmountChange(event: Event) {
+    const target = event.target as HTMLInputElement;
+    const parsedValue = parseInt(target.value);
+    this.amount = parsedValue;
+  }
 }
